c1cb04e6825251311cabd37d8df6f167043e5d72
OyamiKoroto <hhx.xxm@gmail.com>
2016-12-28 13:22:27 +0800

    DS Init

---
+++ DanmakuSimulator/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DanmakuSimulator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.Black);
+            g.DrawRectangle(new Pen(Color.Gray), 0, 0, 100, 100);
+        }
+    }
+}
